eed2254d332c8bbd25d4b854d17824bf2c0a330c
Paolo Landeros <landerospaolo@gmail.com>
2023-10-31 14:19:00 -0300

    fixed

--- el_ahorcado.py
+++ el_ahorcado.py
@@ -1,52 +1,53 @@
-# Importar funcion choice del modulo random 
+# Importar función choice del modulo random 
 from random import choice
-# importamos el diccionario que contiene las palabras secretas
+# Importamos el diccionario que contiene las palabras secretas
 import palabras
 
-# guardamos en variables la categoria y palabra secreta
+# Guardamos en variables la categoría y palabra secreta
 categoria_secreta = choice(list(palabras.palabra_secreta))
 palabra_secreta = choice(list(palabras.palabra_secreta[categoria_secreta]))
-# print(categoria_secreta)
-# print(palabra_secreta)
+print(categoria_secreta)
+print(palabra_secreta)
 
-# creamos una variable que irá formando la palabra y una que mantendra las otras ocultas
+# Creamos una variable que irá formando la palabra y una que mantendrá las otras ocultas
 letras_adivinadas = ['_'] * len(palabra_secreta)
 letras_ocultas = list(palabra_secreta)
 
-# creamos la variable que saludara y dara inicio al juego
+# Creamos la variable que saludara y dará inicio al juego
 def inicio_del_juego():
-    # preguntamos el nombre del usuario
+    # Preguntamos el nombre del usuario
     nombre_usuario = input('Ingresa tu nombre: ')
-    # guardamos la respuesta del usuario y la pasamos a minuscula para evitar problemas
+    # Guardamos la respuesta del usuario y la pasamos a minúscula para evitar problemas
     respuesta_usuario = input(f"""- Hola {nombre_usuario}.
-Bienvenido/a al juego del ahorcado, quieres jugar?
+Bienvenido/a al juego del ahorcado. ¿quieres jugar?
 si/no: """).strip().lower()
-    # un bucle en caso de que conteste algo distinto a si o no
+    # Un bucle en caso de que conteste algo distinto a si o no
     while respuesta_usuario not in ['si', 'no']:
             respuesta_usuario = input(f"""- Hola {nombre_usuario}.
-necesito saber si quieres jugar o no
+Necesito saber si quieres jugar o no
 si/no: """).strip().lower()
-    # ejecutamos la funcion letra_usuario, en caso de querer jugar
+    # Ejecutamos la función letra_usuario, en caso de querer jugar
     letra_usuario(respuesta_usuario, nombre_usuario)
 
-# creamos la funcion para comenzar el juego
+# Creamos la función para comenzar el juego
 def letra_usuario(respuesta, nombre):
     vidas = 6
     if(respuesta == 'si'):
-        print(f"""-Muy bien, comencemos, te recuerdo que tienes 6 vidas.
-{nombre} te dare dos pistas, la palabra tiene un largo de {len(palabra_secreta)} letras
-la palabra se encuentra en la categoria {categoria_secreta}""")
+        print(f"""- Muy bien, comencemos, te recuerdo que tienes 6 vidas.
+{nombre} te daré dos pistas, la palabra tiene un largo de {len(palabra_secreta)} letras
+la palabra se encuentra en la categoría {categoria_secreta}""")
         letras_adivinadas = ['_'] * len(palabra_secreta)
         letras_ocultas = list(palabra_secreta)
+        # Crear un conjunto para almacenar letras acertadas
+        letras_acertadas = set()  
         
-        letras_acertadas = set()  # Crear un conjunto para almacenar letras acertadas
-        
-        # El juego durara hasta que se acaben las vidas o se complete la palabra
+        # El juego durará hasta que se acaben las vidas o se complete la palabra
         while vidas > 0:
             letra = input('- Dime una letra cualquiera: ')
             while not letra.isalpha() or len(letra) != 1:
                 letra = input(f"""{nombre}, te recuerdo que debes ingresar 1 letra
 Dime una letra cualquiera: """)
+            # Se le avisa al jugador si ya adivino esa letra
             if letra in letras_acertadas:
                 print(f"- Ya adivinaste la letra {letra}")
             elif letra in letras_ocultas:
@@ -54,26 +55,29 @@ Dime una letra cualquiera: """)
                 letras_acertadas.add(letra)
                 # Bucle for para ir mostrando las letras adivinadas
                 for i, l in enumerate(letras_ocultas):
-                    # si l es igual a la letra del jugador se actualiza la lista, al mimsmo tiempo se reemplaza por un _
+                    # Si l es igual a la letra del jugador se actualiza la lista, al mismo tiempo se reemplaza por un _
                     # para demostrar que ya no es una letra oculta
                     if l == letra:
                         letras_adivinadas[i] = letra
                         letras_ocultas[i] = '_'
                 # Se imprime la palabra con las letras adivinadas
                 print(' '.join(letras_adivinadas))
+                # Si no quedan letras en la palabra oculta, el juego termina felicitando al jugador
                 if not '_' in letras_adivinadas:
-                    print(f"Felicidades {nombre}, has adivinado la palabra!!")
+                    print(f"¡¡Felicidades {nombre}, has adivinado la palabra!!")
                     break
             else:
+                # Cada vez que falle al adivinar la letra se le descuenta 1 vida
                 vidas -= 1
+                # Si las vidas son 0, se le da un aviso de que perdió y se muestra la palabra
                 if vidas == 0:
                     print(f"""- Era tu última oportunidad, una lástima!
-La palabra era {palabra_secreta}""")
+La palabra era '{palabra_secreta}'""")
                 else:
+                    # En caso de no acertar se le da un aviso con la cantidad de vidas que quedan
                     print(f"""- No has acertado
 {nombre}, te quedan {vidas} vidas""")
-        if not letras_ocultas:
-            print(f"Felicidades {nombre}, has adivinado la palabra!!")
+    # En caso de que no quiera jugar, se le da un aviso
     else:
         print('Que pena, vuelve luego')
 
